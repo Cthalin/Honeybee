c10c14369e2292ec9184053361216e8e9a84c70c
Erik <elidorian@gmail.com>
2018-03-10 23:45:56 +0100

    Ambient Sound added
    inkl DontDestroy Script

---
+++ Assets/Scripts/AmbientAudioScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmbientAudioScript : MonoBehaviour {
+
+    public static AmbientAudioScript instance = null;
+
+    void Awake()
+    {
+        //Check if instance already exists
+        if (instance == null)
+
+            instance = this;
+
+        else if (instance != this)
+
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
