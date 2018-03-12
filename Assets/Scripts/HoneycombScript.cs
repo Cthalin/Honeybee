9f4e894abb4b31b1a09b70c9a908c878079c3026
Erik <elidorian@gmail.com>
2018-03-12 20:29:10 +0100

    refactored fct from honeycomb to tile script

--- Assets/Scripts/HoneycombScript.cs
+++ Assets/Scripts/HoneycombScript.cs
@@ -13,43 +13,10 @@ public class HoneycombScript : MonoBehaviour {
     [SerializeField] private GameObject _fadeCanvas;
 
     [SerializeField] private float _howLongToDo = 0.2f;
-    [SerializeField] private float _howLongToWait = 2f;
-    //[SerializeField] private float _howFarToMove = 0.02f;
+
     [SerializeField] private float _howMuchToAddToIntensity = 0.01f;
 
     [SerializeField] private PostProcessingProfile _profile;
-
-    private Boolean _isBlockedForMove = false;
-    //private LidState _stateOfLid = LidState.CLOSED;
-
-    //private enum LidState
-    //{
-    //    CLOSED,
-    //    PUSHED1,
-    //    PUSHED2,
-    //    PUSHED3,
-    //    OPEN
-    //}
-
-    //public void Move()
-    //{
-    //    StartCoroutine(DoMove());
-    //}
-
-    //IEnumerator DoMove()
-    //{
-    //    float timer = Time.time + _howLongToDo;
-
-    //    while (Time.time < timer)
-    //    {
-    //        _honeycombLid.transform.position = new Vector3(_honeycombLid.transform.position.x, _honeycombLid.transform.position.y, _honeycombLid.transform.position.z + _howFarToMove);
-
-    //        yield return null;
-    //    }
-
-    //    //canvasGroup.interactable = false;
-    //    yield return null;
-    //}
 
     public void ChangePPProfile()
     {
@@ -139,83 +106,8 @@ public class HoneycombScript : MonoBehaviour {
         IntroVignetteReduction();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SendFadeRequestToGameManager()
     {
-
-        //if (Input.GetMouseButtonDown(0))
-        //{
-        //    RaycastHit hit;
-        //    Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        //    if (Physics.Raycast(_ray, out hit, Mathf.Infinity))
-        //    {
-        //        if (hit.collider.tag == "target" && _isBlockedForMove == false)
-        //        {
-        //            //Move();
-        //            ChangePPProfile();
-        //            ChangeLight();
-        //            StartCoroutine(FixedWait());
-        //            ChangeLidState();
-        //        }
-        //    }
-        //}
-
-        if (Input.GetMouseButtonDown(1))
-        {
-            gameManager.GetComponent<GameManager>().FadeIntoWhite(_fadeCanvas);
-        }
+        gameManager.GetComponent<GameManager>().FadeIntoWhite(_fadeCanvas);
     }
-
-    IEnumerator FixedWait()
-    {
-        float timer = Time.time + _howLongToWait;
-
-        while (Time.time < timer)
-        {
-            _isBlockedForMove = true;
-            yield return null;
-        }
-
-        _isBlockedForMove = false;
-        yield return null;
-    }
-
-    //public void PlayAnimation(String animName)
-    //{
-    //    _honeycombLid.GetComponent<Animator>().Play(animName);
-    //}
-
-    //public void ChangeLidState()
-    //{
-    //    switch (_stateOfLid)
-    //    {
-    //        case LidState.CLOSED:
-    //            _stateOfLid = LidState.PUSHED1;
-    //            PlayAnimation("LidWobble");
-    //            break;
-
-    //        case LidState.PUSHED1:
-    //            _stateOfLid = LidState.PUSHED2;
-    //            PlayAnimation("LidPushFirst");
-    //            break;
-
-    //        case LidState.PUSHED2:
-    //            _stateOfLid = LidState.PUSHED3;
-    //            PlayAnimation("LidPushSecond");
-    //            break;
-
-    //        case LidState.PUSHED3:
-    //            _stateOfLid = LidState.OPEN;
-    //            PlayAnimation("LidPushSecond");
-    //            break;
-
-    //        case LidState.OPEN:
-    //            PlayAnimation("LidPushThird");
-    //            gameManager.GetComponent<GameManager>().FadeIntoWhite(_fadeCanvas);
-    //            break;
-
-    //        default: break;
-    //    }
-    //}
 }
--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileScript : MonoBehaviour {
 
     [SerializeField] GameObject[] Tiles = new GameObject[3];
+    [SerializeField] private float _howLongToWait = 2f;
     private int _tileNo;
     private string _tileName;
+    private Boolean _isBlocked = false;
+
 
     private void OnEnable()
     {
@@ -19,28 +23,29 @@ public class TileScript : MonoBehaviour {
         Tiles[tileNo].tag = "target";
     }
 
-    private void IterateOnTiles()
-    {
-
-    }
-
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            RaycastHit _hit;
-            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            if (Physics.Raycast(_ray, out _hit, Mathf.Infinity))
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                if (_hit.collider.tag == "target")
+                if (hit.collider.tag == "target" && !_isBlocked)
                 {
-                    Vanish(_hit.collider.gameObject);
-                    if(_tileNo > 0)
+                    Vanish(hit.collider.gameObject); //Remove Tile
+                    if (_tileNo > 0)
                     {
-                        _tileNo -= 1;
+                        _tileNo -= 1; //Iterate Tile number
                         _tileName = Tiles[_tileNo].name;
                         SetTileAsTarget(_tileNo);
+                        StartCoroutine(FixedWait());
+                    }
+                    else
+                    {
+                        //Last Tile removed
+                        GetComponent<HoneycombScript>().SendFadeRequestToGameManager();
                     }
                 }
             }
@@ -67,6 +72,20 @@ public class TileScript : MonoBehaviour {
         }
 
         //canvasGroup.interactable = false;
+        yield return null;
+    }
+
+    IEnumerator FixedWait()
+    {
+        float timer = Time.time + _howLongToWait;
+
+        while (Time.time < timer)
+        {
+            _isBlocked = true;
+            yield return null;
+        }
+
+        _isBlocked = false;
         yield return null;
     }
 }
