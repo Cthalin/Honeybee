645e1c84b23df5c7fa79cb6e26a955b1da05e741
Cthalin <elidorian@gmail.com>
2018-03-13 10:03:11 +0100

    Added audio guide object & script

---
+++ Assets/Scripts/GuideScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuideScript : MonoBehaviour {
+
+    [SerializeField] AudioClip[] _guideSteps = new AudioClip[7];
+    [SerializeField] private float _initWait = 3f;
+    private AudioSource _audioSource;
+    private int _clipIndex = 0;
+    private bool _isWaiting;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        changeAudioClip(_clipIndex); //Start with first guide clip
+        _audioSource.PlayDelayed(_initWait); //Play after init timer for slower player introduction
+    }
+
+    public void changeAudioClip(int clipIndex)
+    {
+        _audioSource.clip = _guideSteps[_clipIndex];
+    }
+
+    IEnumerator FixedWait()
+    {
+        float timer = Time.time + _initWait;
+
+        while (Time.time < timer)
+        {
+            _isWaiting = true;
+            yield return null;
+        }
+
+        _isWaiting = false;
+        yield return null;
+    }
+}
