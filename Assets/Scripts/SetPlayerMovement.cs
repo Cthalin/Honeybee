7f60e7193150df0c11da69a663cac214b2e6322d
Erik <elidorian@gmail.com>
2018-04-02 18:55:08 +0200

    disable movement

---
+++ Assets/Scripts/SetPlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class SetPlayerMovement : MonoBehaviour {
+
+    void Awake()
+    {
+        VRTK_SDKManager.instance.AddBehaviourToToggleOnLoadedSetupChange(this);
+    }
+    protected virtual void OnEnable()
+    {
+        var activeCameraRig = GameObject.FindGameObjectWithTag("CameraRig");
+    }
+    protected virtual void OnDestroy()
+    {
+        VRTK_SDKManager.instance.RemoveBehaviourToToggleOnLoadedSetupChange(this);
+    }
+}
