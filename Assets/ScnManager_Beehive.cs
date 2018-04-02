83ca933e1e016fc124007ab1800f859a9c3e16f0
LadyLazorcorn <susannbaumgart@gmail.com>
2018-04-02 14:27:02 +0200

    sounds added

---
+++ Assets/ScnManager_Beehive.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScnManager_Beehive : MonoBehaviour {
+
+	public CanvasGroup canvasGroup;
+	public GameObject canvasFadeIn;
+
+	// Use this for initialization
+	void Start () {
+		canvasFadeIn.SetActive (true);
+		canvasGroup.GetComponent<FadeScript>().FadeOut ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/SoundManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource ambient;
+
+	[Header("Guide Sounds")]
+	public AudioSource guide1;
+	public AudioSource guide2;
+	public AudioSource guide3;
+	public AudioSource guide4;
+	public AudioSource guide5;
+	public AudioSource guide6;
+	public AudioSource guide7;
+	public AudioSource guide8;
+	public AudioSource guide9;
+	public AudioSource guide10;
+
+	[Header("Hint Sounds")] 
+	//played when user is doing something out of the guided experience
+	public AudioSource hint1;
+	public AudioSource hint2;
+	public AudioSource hint3;
+
+	[Header("Additional Info Sounds")]
+	//played after this scene ends and user wnts to lern even more
+	public AudioSource info1;
+	public AudioSource info2;
+	public AudioSource info3;
+	public AudioSource info4;
+	public AudioSource info5;
+	public AudioSource info6;
+
+
+
+}
